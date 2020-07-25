e9cf01a877834cf2c409a9054342ba577ee3da5f
Jacob Robertson <g.jacob.robertson@gmail.com>
2020-07-25 15:08:27 -0400

    remove secret key base config

--- config/releases.exs
+++ config/releases.exs
@@ -2,9 +2,7 @@ import Config
 
 host = System.fetch_env!("HOST")
 port = System.fetch_env!("PORT") |> String.to_integer()
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 config :rogue, RogueWeb.Endpoint,
   url: [host: nil, port: 443],
   http: [port: {:system, "PORT"}]
-  secret_key_base: secret_key_base
